522a5bf6d48c0aa6eecc7dde609a65a2f0b22cb7
Jack Kawell (surface) <jgkawell@outlook.com>
2017-12-09 09:05:18 -0600

    This is a new branch and commit for a working Facebook authentication. Fixes package issues with past authentication branches.

--- JudgementZone/UI/MenuPages/MainMenuPage.xaml.cs
+++ JudgementZone/UI/MenuPages/MainMenuPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using JudgementZone.Services;
 using Xamarin.Forms;
 
@@ -25,15 +24,22 @@ namespace JudgementZone.UI
         {
             Device.BeginInvokeOnMainThread(async () =>
             {
+                if (S_GameConnector.Connector.IsConnected())
+                {
+                    await S_GameConnector.Connector.SendQuestionSyncRequest();
+                    await MenuLogo.AnimateColorLoadFromZeroAsync(E_LogoColor.Random, 0.975, 300);
+                    MenuLogo.IsAnimating = false;
+                }
+
                 // Check if should animate
                 var animationEnabled = !MenuLogo.IsAnimating;
                 if (animationEnabled)
                 {
-					MenuLogo.IsAnimating = true;
+                    MenuLogo.IsAnimating = true;
                 }
 
                 // Check if already connected
-                if (S_GameConnector.Connector.IsConnected())
+                if (S_GameConnector.Connector.IsConnected() || S_GameConnector.Connector.authenticated)
                 {
                     if (animationEnabled)
                     {
@@ -44,38 +50,32 @@ namespace JudgementZone.UI
                 else
                 {
                     var firstAnimComplete = false;
-                    while (!S_GameConnector.Connector.IsConnected())
+                    ////HACK
+                    while (!S_GameConnector.Connector.IsConnected() || !S_GameConnector.Connector.authenticated)
                     {
                         // Run this at beginning of loop so that connection can not restore during alert,
                         // thereby skipping the callback animation and leaving MenuLogo in a permenant unusable state
                         if (firstAnimComplete)
                         {
-							Console.WriteLine("CONNECTION ERROR");
-							await DisplayAlert("Connection Error", "Could not connect to server. Press ok to try again.", "OK");
+                            Console.WriteLine("CONNECTION ERROR");
+                            await DisplayAlert("Connection Error", "Could not connect to server. Press ok to try again.", "OK");
                         }
-
-                        var connectionTask = S_GameConnector.Connector.StartConnectionAsync();
 
                         if (animationEnabled && !firstAnimComplete)
                         {
                             var animTask = MenuLogo.AnimateColorLoadToZeroAsync(0.975, 300);
                             await animTask;
                             firstAnimComplete = true;
-                            await connectionTask;
-                        }
-                        else
-                        {
-                            await connectionTask;
                         }
 
-                        if (S_GameConnector.Connector.IsConnected())
-                        {
-                            await S_GameConnector.Connector.SendQuestionSyncRequest();
-                            await MenuLogo.AnimateColorLoadFromZeroAsync(E_LogoColor.Random, 0.975, 300);
-                            MenuLogo.IsAnimating = false;
-                        }
+                        //if (S_GameConnector.Connector.IsConnected())
+                        //{
+                        //    await S_GameConnector.Connector.SendQuestionSyncRequest();
+                        //    await MenuLogo.AnimateColorLoadFromZeroAsync(E_LogoColor.Random, 0.975, 300);
+                        //    MenuLogo.IsAnimating = false;
+                        //}
                     }
-				}
+                }
             });
         }
 
@@ -90,19 +90,23 @@ namespace JudgementZone.UI
                 if (!_uiLock)
                 {
                     _uiLock = true;
-					if (S_GameConnector.Connector.IsConnected())
-					{
-						if (!MenuLogo.IsAnimating) {
-							MenuLogo.IsAnimating = true;
-							await MenuLogo.AnimateColorFadeAsync(crossFade: true, pulseScale: 0.985, duration: 200);
-							MenuLogo.IsAnimating = false;
-						}
-						await Navigation.PushAsync(new StartGamePage());
-					}
-					else
-					{
-						await DisplayAlert("Connecting to Server", "Please be patient while we connect you to the server.", "OK");
-					}
+                    if (S_GameConnector.Connector.IsConnected())
+                    {
+                        if (!MenuLogo.IsAnimating)
+                        {
+                            MenuLogo.IsAnimating = true;
+                            await MenuLogo.AnimateColorFadeAsync(crossFade: true, pulseScale: 0.985, duration: 200);
+                            MenuLogo.IsAnimating = false;
+                        }
+                        await Navigation.PushAsync(new StartGamePage());
+                    }
+                    else
+                    {
+                        if (S_GameConnector.Connector.authenticated)
+                        {
+                            await DisplayAlert("Authenticated", "Successfully authenticated but couldn't connect to the server.", "OK");
+                        }
+                    }
                     _uiLock = false;
                 }
             });
@@ -110,89 +114,71 @@ namespace JudgementZone.UI
 
         void JoinGameButtonClicked(object sender, EventArgs e)
         {
-			Device.BeginInvokeOnMainThread(async () =>
-			{
+            Device.BeginInvokeOnMainThread(async () =>
+            {
                 if (!_uiLock)
                 {
                     _uiLock = true;
-					if (S_GameConnector.Connector.IsConnected())
-					{
-						if (!MenuLogo.IsAnimating)
-						{
-							MenuLogo.IsAnimating = true;
-							await MenuLogo.AnimateColorFadeAsync(crossFade: true, pulseScale: 0.985, duration: 200);
-							MenuLogo.IsAnimating = false;
-						}
-						await Navigation.PushAsync(new JoinGamePage());
-					}
-					else
-					{
-						await DisplayAlert("Connecting to Server", "Please be patient while we connect you to the server.", "OK");
-					}
+                    if (S_GameConnector.Connector.IsConnected())
+                    {
+                        if (!MenuLogo.IsAnimating)
+                        {
+                            MenuLogo.IsAnimating = true;
+                            await MenuLogo.AnimateColorFadeAsync(crossFade: true, pulseScale: 0.985, duration: 200);
+                            MenuLogo.IsAnimating = false;
+                        }
+                        await Navigation.PushAsync(new JoinGamePage());
+                    }
+                    else
+                    {
+                        if (S_GameConnector.Connector.authenticated)
+                        {
+                            await DisplayAlert("Authenticated", "Successfully authenticated but couldn't connect to the server.", "OK");
+                        }
+                    }
                     _uiLock = false;
                 }
-			});
+            });
         }
 
-		void AchievementsButtonClicked(object sender, EventArgs e)
-		{
-			Device.BeginInvokeOnMainThread(async () =>
-			{
+        void AchievementsButtonClicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
                 if (!_uiLock)
                 {
                     _uiLock = true;
-					if (S_GameConnector.Connector.IsConnected())
-					{
-						await DisplayAlert("Achievements Not Implemented", "Try again in a later build.", "OK");
-					}
-					else
-					{
-						await DisplayAlert("Connecting to Server", "Please be patient while we connect you to the server.", "OK");
-					}
+                    await DisplayAlert("Achievements Not Implemented", "Try again in a later build.", "OK");
                     _uiLock = false;
                 }
-			});
-		}
+            });
+        }
 
-		void SettingsButtonClicked(object sender, EventArgs e)
-		{
-			Device.BeginInvokeOnMainThread(async () =>
-			{
+        void SettingsButtonClicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
                 if (!_uiLock)
                 {
                     _uiLock = true;
-					if (S_GameConnector.Connector.IsConnected())
-					{
-						await DisplayAlert("Settings Not Implemented", "Try again in a later build.", "OK");
-					}
-					else
-					{
-						await DisplayAlert("Connecting to Server", "Please be patient while we connect you to the server.", "OK");
-					}
+                    await DisplayAlert("Settings Not Implemented", "Try again in a later build.", "OK");
                     _uiLock = false;
                 }
-			});
-		}
+            });
+        }
 
-		void AboutButtonClicked(object sender, EventArgs e)
-		{
-			Device.BeginInvokeOnMainThread(async () =>
-			{
+        void AboutButtonClicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
                 if (!_uiLock)
                 {
                     _uiLock = true;
-					if (S_GameConnector.Connector.IsConnected())
-					{
-						await DisplayAlert("About Not Implemented", "Try again in a later build.", "OK");
-					}
-					else
-					{
-						await DisplayAlert("Connecting to Server", "Please be patient while we connect you to the server.", "OK");
-					}
+                    await DisplayAlert("About Not Implemented", "Try again in a later build.", "OK");
                     _uiLock = false;
                 }
-			});
-		}
+            });
+        }
 
         #endregion
 
